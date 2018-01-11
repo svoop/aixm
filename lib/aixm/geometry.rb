c264f392394a7c65cb524ffab97f2d364e701ba6
Sven Schwyn <schwyn@bitcetera.com>
2018-01-11 20:42:19 +0100

    Improve Geometry and add more tests

--- lib/aixm/geometry.rb
+++ lib/aixm/geometry.rb
@@ -10,8 +10,20 @@ module AIXM
       @result_array = horizontals
     end
 
-    def closed?
-      @result_array.first.is_a?(AIXM::Horizontal::Point) &&
+    def valid?
+      circle? || closed_shape?
+    end
+
+    private
+
+    def circle?
+      @result_array.size == 1 &&
+        @result_array.first.is_a?(AIXM::Horizontal::Circle)
+    end
+
+    def closed_shape?
+      @result_array.size >= 3 &&
+        !@result_array.any? { |h| h.is_a?(AIXM::Horizontal::Circle) } &&
         @result_array.last.is_a?(AIXM::Horizontal::Point) &&
         @result_array.first.xy == @result_array.last.xy
     end
--- spec/lib/aixm/geometry_spec.rb
+++ spec/lib/aixm/geometry_spec.rb
@@ -1,20 +1,132 @@
 require_relative '../../spec_helper'
 
 describe AIXM::Geometry do
-  subject do
-    AIXM::Geometry.new.tap do |geometry|
-      geometry << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 11, long: 22))
-      geometry << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 22, long: 33))
-      geometry << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 33, long: 44))
+  context "singularity" do
+    subject do
+      AIXM::Geometry.new
+    end
+
+    it "must fail validation" do
+      subject.wont_be :circle?
+      subject.wont_be :closed_shape?
+      subject.wont_be :valid?
     end
   end
 
-  it "must recognize unclosed" do
-    subject.wont_be :closed?
+  context "point" do
+    subject do
+      AIXM::Geometry.new.tap do |geometry|
+        geometry << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 11, long: 22))
+      end
+    end
+
+    it "must fail validation" do
+      subject.wont_be :circle?
+      subject.wont_be :closed_shape?
+      subject.wont_be :valid?
+    end
   end
 
-  it "must recognize closed" do
-    subject << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 11, long: 22))
-    subject.must_be :closed?
+  context "line" do
+    subject do
+      AIXM::Geometry.new.tap do |geometry|
+        geometry << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 11, long: 22))
+        geometry << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 22, long: 33))
+      end
+    end
+
+    it "must fail validation" do
+      subject.wont_be :circle?
+      subject.wont_be :closed_shape?
+      subject.wont_be :valid?
+    end
   end
+
+  context "polygon" do
+    subject do
+      AIXM::Geometry.new.tap do |geometry|
+        geometry << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 11, long: 22))
+        geometry << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 22, long: 33))
+      end
+    end
+
+    it "must recognize unclosed" do
+      subject.wont_be :circle?
+      subject.wont_be :closed_shape?
+      subject.wont_be :valid?
+    end
+
+    it "must recognize closed" do
+      subject << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 11, long: 22))
+      subject.wont_be :circle?
+      subject.must_be :closed_shape?
+      subject.must_be :valid?
+    end
+  end
+
+  context "arc" do
+    subject do
+      AIXM::Geometry.new.tap do |geometry|
+        geometry << AIXM::Horizontal::Arc.new(xy: AIXM::XY.new(lat: 11, long: 22), center_xy: AIXM::XY.new(lat: 10, long: 20), clockwise: true)
+        geometry << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 22, long: 33))
+      end
+    end
+
+    it "must recognize unclosed" do
+      subject.wont_be :circle?
+      subject.wont_be :closed_shape?
+      subject.wont_be :valid?
+    end
+
+    it "must recognize closed" do
+      subject << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 11, long: 22))
+      subject.wont_be :circle?
+      subject.must_be :closed_shape?
+      subject.must_be :valid?
+    end
+  end
+
+  context "border" do
+    subject do
+      AIXM::Geometry.new.tap do |geometry|
+        geometry << AIXM::Horizontal::Border.new(xy: AIXM::XY.new(lat: 11, long: 22), name: 'foobar')
+        geometry << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 22, long: 33))
+      end
+    end
+
+    it "must recognize unclosed" do
+      subject.wont_be :circle?
+      subject.wont_be :closed_shape?
+      subject.wont_be :valid?
+    end
+
+    it "must recognize closed" do
+      subject << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 11, long: 22))
+      subject.wont_be :circle?
+      subject.must_be :closed_shape?
+      subject.must_be :valid?
+    end
+  end
+
+  context "circle" do
+    subject do
+      AIXM::Geometry.new.tap do |geometry|
+        geometry << AIXM::Horizontal::Circle.new(center_xy: AIXM::XY.new(lat: 11, long: 22), radius: 10)
+      end
+    end
+
+    it "must pass validation" do
+      subject.must_be :circle?
+      subject.wont_be :closed_shape?
+      subject.must_be :valid?
+    end
+
+    it "must fail validation when additional elements are present" do
+      subject << AIXM::Horizontal::Point.new(xy: AIXM::XY.new(lat: 11, long: 22))
+      subject.wont_be :circle?
+      subject.wont_be :closed_shape?
+      subject.wont_be :valid?
+    end
+  end
+
 end
