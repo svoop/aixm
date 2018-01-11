9ea034843cedd6a613a95d5e3a554eb310bd1c25
Sven Schwyn <schwyn@bitcetera.com>
2018-01-11 15:39:09 +0100

    Group Z tests

--- spec/lib/aixm/z_spec.rb
+++ spec/lib/aixm/z_spec.rb
@@ -1,43 +1,51 @@
 require_relative '../../spec_helper'
 
 describe AIXM::Z do
-  it "must parse valid Q code" do
-    subject = AIXM::Z.new(alt: 111, code: :QNH)
-    subject.alt.must_equal 111
-    subject.code.must_equal :QNH
+  describe :initialize do
+    it "must parse valid Q code" do
+      subject = AIXM::Z.new(alt: 111, code: :QNH)
+      subject.alt.must_equal 111
+      subject.code.must_equal :QNH
+    end
+
+    it "won't parse invalid Q code" do
+      -> { AIXM::Z.new(alt: 111, code: :FOO) }.must_raise ArgumentError
+    end
   end
 
-  it "won't parse invalid Q code" do
-    -> { AIXM::Z.new(alt: 111, code: :FOO) }.must_raise ArgumentError
+  describe :== do
+    it "recognizes objects with identical altitude and Q code as equal" do
+      a = AIXM::Z.new(alt: 111, code: :QNH)
+      b = AIXM::Z.new(alt: 111, code: :QNH)
+      a.must_equal b
+    end
+
+    it "recognizes objects with different altitude or Q code as unequal" do
+      a = AIXM::Z.new(alt: 111, code: :QNH)
+      b = AIXM::Z.new(alt: 222, code: :QNH)
+      a.wont_equal b
+    end
+
+    it "recognizes objects of different class as unequal" do
+      a = AIXM::Z.new(alt: 111, code: :QNH)
+      b = :oggy
+      a.wont_equal b
+    end
   end
 
-  it "recognizes objects with identical altitude and Q code as equal" do
-    a = AIXM::Z.new(alt: 111, code: :QNH)
-    b = AIXM::Z.new(alt: 111, code: :QNH)
-    a.must_equal b
+  describe :ground? do
+    it "must detect ground" do
+      AIXM::Z.new(alt: 0, code: :QFE).must_be :ground?
+      AIXM::Z.new(alt: 111, code: :QFE).wont_be :ground?
+      AIXM::Z.new(alt: 0, code: :QNH).wont_be :ground?
+    end
   end
 
-  it "recognizes objects with different altitude or Q code as unequal" do
-    a = AIXM::Z.new(alt: 111, code: :QNH)
-    b = AIXM::Z.new(alt: 222, code: :QNH)
-    a.wont_equal b
-  end
-
-  it "recognizes objects of different class as unequal" do
-    a = AIXM::Z.new(alt: 111, code: :QNH)
-    b = :oggy
-    a.wont_equal b
-  end
-
-  it "must detect ground" do
-    AIXM::Z.new(alt: 0, code: :QFE).must_be :ground?
-    AIXM::Z.new(alt: 111, code: :QFE).wont_be :ground?
-    AIXM::Z.new(alt: 0, code: :QNH).wont_be :ground?
-  end
-
-  it "must return correct base" do
-    AIXM::Z.new(alt: 0, code: :QFE).base.must_equal :ASFC
-    AIXM::Z.new(alt: 0, code: :QNH).base.must_equal :AMSL
-    AIXM::Z.new(alt: 0, code: :QNE).base.must_equal :AMSL
+  describe :base do
+    it "must return correct base" do
+      AIXM::Z.new(alt: 0, code: :QFE).base.must_equal :ASFC
+      AIXM::Z.new(alt: 0, code: :QNH).base.must_equal :AMSL
+      AIXM::Z.new(alt: 0, code: :QNE).base.must_equal :AMSL
+    end
   end
 end
