bdfe94ae42894eb37165e5e31c84350808fb6b61
Sven Schwyn <schwyn@bitcetera.com>
2019-09-29 20:00:27 +0200

    Add rake task to update OFMX schema

--- rakefile.rb
+++ rakefile.rb
@@ -9,14 +9,28 @@ Rake::TestTask.new do |t|
   t.warning = true
 end
 
-desc "Run local YARD documentation server"
-task :yard do
-  `rm -rf ./.yardoc`
-  Thread.new do
-    sleep 2
-    `open http://localhost:8808`
+namespace :schema do
+  desc "Update OFMX schema"
+  task :update do
+    `rm -rf schemas/ofmx/0/*`
+    `wget http://schema.openflightmaps.org/0/OFMX-CSV-Obstacle.json -P schemas/ofmx/0/ -q --show-progress`
+ 	  `wget http://schema.openflightmaps.org/0/OFMX-CSV.json -P schemas/ofmx/0/ -q --show-progress`
+ 	  `wget http://schema.openflightmaps.org/0/OFMX-DataTypes.xsd -P schemas/ofmx/0/ -q --show-progress`
+ 	  `wget http://schema.openflightmaps.org/0/OFMX-Features.xsd -P schemas/ofmx/0/ -q --show-progress`
+ 	  `wget http://schema.openflightmaps.org/0/OFMX-Snapshot.xsd -P schemas/ofmx/0/ -q --show-progress`
   end
-  `yard server -r`
-end  
+end
+
+namespace :yard do
+  desc "Run local YARD documentation server"
+  task :server do
+    `rm -rf ./.yardoc`
+    Thread.new do
+      sleep 2
+      `open http://localhost:8808`
+    end
+    `yard server -r`
+  end
+end
 
 task default: :test
