c54d3f1409dc036db398448200f447ffa4869d2b
Sven Schwyn <schwyn@bitcetera.com>
2022-02-03 20:11:55 +0100

    Streamline spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-$VERBOSE = nil   # silence warnings
-
 gem 'minitest'
 
 require 'pathname'
@@ -9,8 +7,8 @@ require Pathname(__dir__).join('..', 'lib', 'aixm')
 
 require 'minitest/sound'
 require 'minitest/sound/reporter'
-Minitest::Sound.success = Pathname(__dir__).join('sounds/success.mp3').to_s
-Minitest::Sound.failure = Pathname(__dir__).join('sounds/failure.mp3').to_s
+Minitest::Sound.success = Pathname(__dir__).join('sounds', 'success.mp3').to_s
+Minitest::Sound.failure = Pathname(__dir__).join('sounds', 'failure.mp3').to_s
 require 'minitest/reporters'
 Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new, Minitest::Sound::Reporter.new]
 
